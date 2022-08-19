6d6e43a28cbc9222703e9b3e26873f421be42619
Ruslan Kolosovskyi <ruslan.kolosovskyi@gmail.com>
2022-08-19 16:27:07 +0300

    SPM support has been added.

--- Sources/GridCollectionViewLayout.swift
+++ Sources/GridCollectionViewLayout.swift
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#if canImport(UIKit)
+
 import UIKit
 
 /**
@@ -287,3 +289,5 @@ extension GridCollectionViewLayout {
         return itemSize.height+itemSpacing
     }
 }
+
+#endif
